43293f33ad751bcb976ae6ac72c5bd16a15d5749
KayphaB <kayphajoseph@gmail.com>
2025-05-15 10:55:47 -0700

    player controller & camerafollow

---
+++ Legend of 5th/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float originX;
+    public float originY;
+    public float screenLengthX;
+    public float screenLengthY;
+    public int speedDecayFactor;
+    private Vector3 target;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //find target (the screen the player is in and the camera should be)
+        target = new Vector3(
+            Mathf.Round(player.transform.position.x / screenLengthX) * screenLengthX, 
+            Mathf.Round(player.transform.position.y / screenLengthY) * screenLengthY, 
+        0);
+
+        //find average between target screen position and current position and go to it
+        transform.position = new Vector3 (
+            (transform.position.x + target.x * speedDecayFactor) / (speedDecayFactor + 1), 
+            (transform.position.y + target.y * speedDecayFactor) / (speedDecayFactor + 1), 
+        -10);
+    }
+}
---
+++ Legend of 5th/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.up * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(transform.right * Time.deltaTime * -speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(transform.up * Time.deltaTime * -speed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * Time.deltaTime * speed);
+        }
+    }
+}
