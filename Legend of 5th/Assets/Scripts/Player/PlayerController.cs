a97c62a647e79049fd3bf76132d464ce7fe92b10
Stewy100 <153382@student.vansd.org>
2025-05-16 10:53:35 -0700

    dungeon 2 tweaks

--- Legend of 5th/Assets/Scripts/Player/PlayerController.cs
+++ Legend of 5th/Assets/Scripts/Player/PlayerController.cs
@@ -12,21 +12,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.Translate(transform.up * Time.deltaTime * speed);
-        }
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(transform.right * Time.deltaTime * -speed);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(transform.up * Time.deltaTime * -speed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(transform.right * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.up * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(transform.up * Time.deltaTime * -speed);
         }
     }
 }
