fa180deb3370a3890b81bb0de7a900df9788fc0a
KayphaB <kayphajoseph@gmail.com>
2025-05-16 14:10:06 -0700

    player animations

--- Legend of 5th/Assets/Scripts/Player/PlayerController.cs
+++ Legend of 5th/Assets/Scripts/Player/PlayerController.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+
+    private Animator anim;
+    public int direction;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        anim.speed = 1;
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(transform.right * Time.deltaTime * -speed);
+            direction = 1;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(transform.right * Time.deltaTime * speed);
+            direction = 2;
         }
         else if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(transform.up * Time.deltaTime * speed);
+            direction = 3;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(transform.up * Time.deltaTime * -speed);
+            direction = 4;
         }
+        else
+        {
+            //if no walking keys pressed, stop animation
+            anim.speed = 0;
+        }
+
+        //update the animation's variables
+        anim.SetInteger("direction", direction);
     }
 }
