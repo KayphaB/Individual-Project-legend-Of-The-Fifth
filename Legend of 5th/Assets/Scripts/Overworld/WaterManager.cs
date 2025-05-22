a67f52fc18826e66bd69f073fd61bc609cbbb479
KayphaB <kayphajoseph@gmail.com>
2025-05-22 10:23:40 -0700

    dungeon 1 work

---
+++ Legend of 5th/Assets/Scripts/Overworld/WaterManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class WaterManager : MonoBehaviour
+{
+    public bool hasRaft;
+    private TilemapCollider2D collider;
+    void Start()
+    {
+        collider = GetComponent<TilemapCollider2D>();
+    }
+
+    void Update()
+    {
+        //disable water collisions if the player has the raft
+        collider.enabled = (!hasRaft);
+    }
+}
