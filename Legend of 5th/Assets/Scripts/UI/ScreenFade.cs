26f32aaf3a45c4d2ef6921765bb162550c9cb5f5
KayphaB <kayphajoseph@gmail.com>
2025-05-28 14:13:16 -0700

    fade function

---
+++ Legend of 5th/Assets/Scripts/UI/ScreenFade.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFade : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public float delay;
+    private float timer;
+    private int stage;
+    public bool on;
+    private Image image;
+    private Color alphaChange;
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+    void FixedUpdate()
+    {
+        timer += 1;
+        if (timer >= delay)
+        {
+            timer = 0;
+            if (on && stage != 4)
+            {
+                stage += 1;
+            }
+            else if (!on && stage != 0)
+            {
+                stage += -1;
+            }
+        }
+
+        image.sprite = sprites[stage];
+
+        if (stage == 0)
+        {
+            alphaChange.a = 0;
+        }
+        else
+        {
+            alphaChange.a = 255;
+        }
+        image.color = alphaChange;
+    }
+}
