9b4b384866a5bdf06f60bd0699a48301c0576b3e
KayphaB <197577@student.vansd.org>
2025-06-02 17:44:46 -0700

    Mini map fnished

---
+++ Legend of 5th/Assets/Scripts/UI/MiniMap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    private RectTransform rt;
+    public GameObject player;
+
+    public float offsetX;
+    public float offsetY;
+    public float screenLengthX;
+    public float screenLengthY;
+    public float caveX;
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (player.transform.position.x < caveX)
+        {
+            rt.anchoredPosition = new Vector3(
+                Mathf.Round(player.transform.position.x / screenLengthX) * 70 + offsetX,
+                Mathf.Round(player.transform.position.y / screenLengthY) * 42 + offsetY,
+                0);
+        }
+    }
+}
