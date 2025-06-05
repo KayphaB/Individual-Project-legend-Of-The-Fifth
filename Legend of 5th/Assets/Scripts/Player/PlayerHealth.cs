ec8d9456143ee120390ca05936b655750822d140
KayphaB <kayphajoseph@gmail.com>
2025-06-05 14:15:14 -0700

    player take damage

--- Legend of 5th/Assets/Scripts/Player/PlayerHealth.cs
+++ Legend of 5th/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,12 +7,28 @@ public class PlayerHealth : MonoBehaviour
     public int maxHP = 3;
     public float HP = 3;
     
+    private float immuneFrames;
+
     void Update()
     {
         //cap health at the max
         if (HP > maxHP)
         {
             HP = maxHP;
+        }
+
+        if (immuneFrames > 0)
+        {
+            immuneFrames -= 1;
+        }
+    }
+
+    void OnCollisionEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy") && immuneFrames == 0)
+        {
+            HP -= 0.5f;
+            immuneFrames = 50;
         }
     }
 }
